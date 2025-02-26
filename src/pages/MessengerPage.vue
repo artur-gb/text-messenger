4c54fcfb1c946a8eb3c37789017c059a42075fc0
arthur-gb <clov77@yahoo.com>
2025-02-26 17:11:52 +0400

    create initial files

---
+++ src/pages/MessengerPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <message-blob-component
+    sender="ewwwv fe"
+    text="dkemw ewfjnwfnmd vnjwnfqkj"
+    date="today"
+  />
+</template>
+
+<script setup lang="ts">
+import MessageBlobComponent from "../components/MessageBlobComponent.vue";
+
+const messages = [
+  {
+    sender: "iefje HHHNNfe",
+    text: "dreaming aboutewv",
+    date: "today",
+  },
+  {
+    sender: "me",
+    text: "okay!",
+    date: "today",
+  },
+  {
+    sender: "iefje HHHNNfe",
+    text: "War and Peace begins in the Russian city of St. Petersburg in 1805, as fear of Napoleon’s ongoing war making begins to set in. Most of the characters are introduced at a party, including Pierre Bezukhov, Andrey Bolkonsky, and the Kuragin and Rostov families. Much of the novel focuses on the interactions between the Bezukhovs, Bolkonskys, and the Rostovs. After their introduction, Andrey Bolkonsky and Nikolay Rostov go",
+    date: "today",
+  },
+];
+</script>
